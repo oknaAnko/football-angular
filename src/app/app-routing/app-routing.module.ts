8427a8bf8141f72c48e4d59245085eb6a4256eab
Anko <anna.jedrzejko@op.pl>
2021-10-14 20:11:31 +0200

    feat: Create ErrorPage with routing

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RankingPageComponent } from '../ranking-page/ranking-page.component';
 import { HomePageComponent } from '../home-page/home-page.component';
+import { ErrorPageComponent } from '../error-page/error-page.component';
 
 const routes: Routes = [
   {
@@ -12,6 +13,7 @@ const routes: Routes = [
     path: 'ranking',
     component: RankingPageComponent,
   },
+  { path: '**', component: ErrorPageComponent },
 ];
 
 @NgModule({
