62d7a7d40244340f89ae9ab85203c8e4b58817c3
Anko <anna.jedrzejko@op.pl>
2021-10-16 17:09:24 +0200

    feat: Add path to routes

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -5,6 +5,7 @@ import { HomePageComponent } from '../home-page/home-page.component';
 import { ErrorPageComponent } from '../error-page/error-page.component';
 
 const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
   {
     path: 'home',
     component: HomePageComponent,
